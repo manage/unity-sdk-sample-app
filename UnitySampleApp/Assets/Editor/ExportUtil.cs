3a1988ee5628a49d02f979b72538f9b645dcedac
andrey.bolshakov <andrey_bolshakov@MacBook-Pro-citrus.local>
2015-02-24 17:28:17 +0200

    Added CI script.

---
+++ UnitySampleApp/Assets/Editor/ExportUtil.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class ExportUtil  {
+	[MenuItem("Assets/Export Package")]
+	public static void ExportPlugin(){
+		string path = "Assets/Plugins/ManagePlugin";
+		AssetDatabase.ExportPackage (path, "Plugin.unitypackage", ExportPackageOptions.Default);
+
+	}
+}
